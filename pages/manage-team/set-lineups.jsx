e36344ac74f7956f60e05913c3349539a03af06f
savbeM <mark.selihov2458@gmail.com>
2022-02-14 21:37:03 +0300

    stylized set-lineups

--- pages/manage-team/set-lineups.jsx
+++ pages/manage-team/set-lineups.jsx
@@ -9,6 +9,25 @@ const SContainer = styled(Container)`
       width: 25px;
     }
 `
+const CircleButtons = styled(Col)`
+   
+   max-width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column
+`
+
+const TopSelectedCard = styled(Col)`
+    max-width: 900px
+`
+
+const SelectedPlayingCard = styled(Col)`
+    width:500px;
+    margin: 0 auto;
+    display:flex;
+    justify-content: space-around;
+`
 
 const ActionButton = styled(Button)`
   width: 100px;
@@ -18,66 +37,68 @@ export default function SetLineup() {
     return <SContainer fluid className='p-3'>
         <Row className='justify-content-between'>
             <Col className='col-auto'>
-                <FontAwesomeIcon icon={faArrowLeft} />
+                <FontAwesomeIcon icon={faArrowLeft}/>
             </Col>
             <Col className='col-auto'>
-                <FontAwesomeIcon icon={faTimes} />
+                <FontAwesomeIcon icon={faTimes}/>
             </Col>
         </Row>
         <Row>
-            <Col xs={1}>
-                <CircleBtn className='mb-2 first' variant='outline-primary'>1</CircleBtn>
+            <CircleButtons xs={1}>
+                <CircleBtn className='mb-2 ' variant='outline-primary'>1</CircleBtn>
                 <CircleBtn className='mb-2'>2</CircleBtn>
                 <CircleBtn className='mb-2'>3</CircleBtn>
                 <CircleBtn className='mb-2'>4</CircleBtn>
-            </Col>
-            <Col xs={8}>
-                <Row className='mt-5 justify-content-around'>
+            </CircleButtons>
+            <TopSelectedCard xs={8}>
+                <SelectedPlayingCard style={{width: "500px", margin: "0 auto"}}>
                     <Col className='col-auto'>
-                        <PlayingCard />
+                        <PlayingCard/>
                     </Col>
                     <Col className='col-auto'>
-                        <PlayingCard />
+                        <PlayingCard/>
                     </Col>
                     <Col className='col-auto'>
-                        <PlayingCard />
+                        <PlayingCard/>
                     </Col>
-                </Row>
-                <Row className='mt-3 mb-5 justify-content-around'>
+                </SelectedPlayingCard>
+                <SelectedPlayingCard className='mt-3 mb-5 justify-content-around'>
                     <Col className='col-auto'>
-                        <PlayingCard className='bottom-left' />
+                        <PlayingCard className='bottom-left'/>
                     </Col>
                     <Col className='col-auto'>
-                        <PlayingCard className='bottom-right' />
+                        <PlayingCard className='bottom-right'/>
                     </Col>
-                </Row>
+                </SelectedPlayingCard>
                 <Row>
                     <Col xs={11}>
                         <Row>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
-                            <Col className='m-0' xs={1}><PlayingCard className='border' /></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
+                            <Col className='m-0' xs={1}><PlayingCard className='border'/></Col>
                         </Row>
                     </Col>
                     <Col xs={1}>
-                        <FontAwesomeIcon icon={faArrowUp} className='mt-3' />
+                        <FontAwesomeIcon icon={faArrowUp} className='mt-3'/>
                         <h4>1/3</h4>
-                        <FontAwesomeIcon icon={faArrowDown} />
+                        <FontAwesomeIcon icon={faArrowDown}/>
                     </Col>
                 </Row>
-            </Col>
+            </TopSelectedCard>
             <Col xs={3}>
-                <PlayingCard className='goalie' />
+                <PlayingCard className='goalie'/>
                 <ActionButton variant='warning' className='mt-4 mb-2'>Auto</ActionButton>
-                <ActionButton variant='danger' className='mt-5 mb-2'>Cancel</ActionButton>
-                <ActionButton variant='success'>Apply</ActionButton>
+                <Row>
+                    <ActionButton variant='danger' className='mt-5 mb-2'>Cancel</ActionButton>
+                    <ActionButton variant='success' className='mt-5 mb-2'>Apply</ActionButton>
+                </Row>
             </Col>
         </Row>
     </SContainer>
