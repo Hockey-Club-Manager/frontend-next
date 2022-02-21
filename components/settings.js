29c98c757729760758db4c74f9cf7e0da224b712
savbeM <mark.selihov2458@gmail.com>
2022-02-21 20:01:54 +0300

    orbitron font

--- components/settings.js
+++ components/settings.js
@@ -8,6 +8,7 @@ export const SModal = styled(Modal)`
   .modal-content {
     background: linear-gradient(0deg, rgba(65, 95, 202, 0.6), rgba(65, 95, 202, 0.6)), radial-gradient(362.94% 105.95% at 50% 53.43%, rgba(255, 255, 255, 0.8) 0%, rgba(0, 56, 255, 0.8) 81.77%);
     border-radius: 26px;
+
   }
   
 `
@@ -22,7 +23,7 @@ const SettingsHeader = styled(Modal.Header)`
 `
 
 const SettingsFont = styled(Col)`
-    font-family: PORT118;
+    font-family: 'Orbitron', sans-serif;
     font-style: normal;
     font-weight: normal;
     font-size: 40px;
@@ -69,10 +70,10 @@ export default function Settings({show, setShow}) {
                 </ModalInputsContainer>
                 <ModalInputsContainer>
                     <Col>
-                        <AcceptButton>Cancel</AcceptButton>
+                        <AcceptButton >Cancel</AcceptButton>
                     </Col>
                     <Col>
-                        <CancelButton>Apply</CancelButton>
+                        <CancelButton >Apply</CancelButton>
                     </Col>
                 </ModalInputsContainer>
             </Form>
