5ea65aeeb033def2de9a7f86ad38e5198570cc95
savbeM <mark.selihov2458@gmail.com>
2022-02-21 20:00:25 +0300

    styled

--- pages/found-player.jsx
+++ pages/found-player.jsx
@@ -2,26 +2,34 @@ import Link from "next/link";
 import {Container, Row, Col, Button} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import {AcceptButton, CancelButton, OrbitronFont} from "../components/styled-components";
+
+const StyledBackground = styled(Container)`
+    background: linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(65, 95, 202, 0.5), rgba(65, 95, 202, 0.5)), radial-gradient(362.94% 105.95% at 50% 53.43%, rgba(255, 255, 255, 0.8) 0%, rgba(0, 56, 255, 0.8) 81.77%);
+    filter: blur(20px);
+    border-radius: 30px;
+`
 
 export default function FoundPlayer() {
     return <Container>
-        <h1 className="my-5 text-center">Username</h1>
+        <OrbitronFont><h1 className="my-5 text-center">Username</h1></OrbitronFont>
         <Row className='justify-content-center'>
-            <Col className='col-3'>
-                <FontAwesomeIcon icon={faStar} />
-            </Col>
+            <Row className='col-3 justify-content-center'>
+                <FontAwesomeIcon style={{fontSize: "200px"}} icon={faStar} />
+            </Row>
         </Row>
         <Row className='my-3'>
             <Col>
-                <h2 className='text-center'>99 88 77</h2>
+                <OrbitronFont><h2 className='text-center'>99 88 77</h2></OrbitronFont>
             </Col>
         </Row>
         <Row className='justify-content-center'>
             <Col className='col-auto'>
-                <Button>Search again</Button>
+                <CancelButton>Search again</CancelButton>
             </Col>
             <Col className='col-auto'>
-                <Link href='/game'><a className='btn btn-primary' type='button' role='button'>Play</a></Link>
+                <Link href='/game'><AcceptButton>Play</AcceptButton></Link>
             </Col>
         </Row>
     </Container>
