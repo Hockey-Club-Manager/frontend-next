121138b37afe55ebd7d2edcda944980e2eed78fa
savbeM <mark.selihov2458@gmail.com>
2022-03-19 20:58:43 +0300

    draggable image menu

--- pages/image-menu-test.jsx
+++ pages/image-menu-test.jsx
@@ -1,55 +1,58 @@
-import {Col, Container, Row} from "react-bootstrap";
-import Draggable from "react-draggable";
 import styled from "styled-components";
-
-const BgRow = styled(Row)`
-  height: 100vh;
-`
-
-const DraggableImgContainer = styled.div`
-  margin-right: 20vw;
-  width: 120vw;
-  height: 100vh;
-  align-content: center;
-`;
+import React, {Component} from "react";
+import {TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
+import {Button} from "react-bootstrap";
 
 const DraggableImg = styled.div`
-  content: url("/menu-image.png");
-  //width: 100%;
-  height: 100%;
+    background: url("/menu-image.png") no-repeat center center fixed;
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;  
 `;
 
-export default function ImageMenu() {
-    // return <Container fluid>
-        {/*<BgRow className='justify-content-center'>*/}
-            {/*<Col className='col-auto align-self-center'>*/}
-            {/*    <Draggable*/}
-            {/*        bounds={'body'}*/}
-            {/*    >*/}
-            {/*        <DraggableImg />*/}
-            {/*    </Draggable>*/}
-            {/*</Col>*/}
-        {/*</BgRow>*/}
-        // </Container>
-    return <DraggableImgContainer id={'draggable-img-container'} className='justify-content-center align-content-center align-items-center'>
-        <Draggable
-            bounds={'#draggable-img-container'}
-        >
-            <DraggableImg />
-        </Draggable>
-    </DraggableImgContainer>
+const MenuButton = styled(Button)`
+    position: relative;
+    top: 190px;
+    left: 150px;
+    width: 200px;
+    background: linear-gradient(177.07deg, #0C2FAD -41.5%, #7391FF 97.56%);
+    border: 6px solid #415FCA;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    margin: 0;
+    padding: 10px;
+    font-family: 'Orbitron';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 35px;
+    line-height: 44px;
+    text-align: center;
+    color: #FFFFFF;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
 
-    // return <Container fluid>
-    // <BgRow className='justify-content-center'>
-    // <Col className='col-auto align-self-center'>
-    //     <DraggableImgContainer id={'draggable-img-container'} className='justify-content-center '>
-    //          <Draggable
-    //              bounds={'#draggable-img-container'}
-    //          >
-    //              <DraggableImg />
-    //          </Draggable>
-    //      </DraggableImgContainer>
-    // </Col>
-    // </BgRow>
-    // </Container>
+
+let doubleClick = {
+    disabled: true
 }
+
+class ImageMenu extends Component {
+    render() {
+        return (
+            <TransformWrapper centerOnInit={true} initialScale={1.6} doubleClick={doubleClick}>
+                <TransformComponent>
+                    <DraggableImg>
+                        <MenuButton>TRADE CARDS</MenuButton>
+                        <MenuButton style={{left: "1150px", top: "390px"}}>MANAGE TEAM</MenuButton>
+                        <MenuButton style={{left: "400px", top: "350px"}}>PLAY GAME</MenuButton>
+                    </DraggableImg>
+                </TransformComponent>
+            </TransformWrapper>
+        );
+    }
+}
+export default ImageMenu
+
+
