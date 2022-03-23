384706a40500fcd453e27ccccf02ca5e6b947764
savbeM <mark.selihov2458@gmail.com>
2022-03-23 19:38:15 +0300

    display flex

--- pages/image-menu-test.jsx
+++ pages/image-menu-test.jsx
@@ -1,27 +1,31 @@
 import styled from "styled-components";
 import React, {Component} from "react";
 import {TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
-import {Button} from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from "@fortawesome/free-solid-svg-icons";
-import { faHockeyPuck } from "@fortawesome/free-solid-svg-icons";
+import {Button, Container} from "react-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCog} from "@fortawesome/free-solid-svg-icons";
+import {faHockeyPuck} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 const DraggableImg = styled.div`
     background: url("/menu-image.png") no-repeat center center fixed;
     background-size: cover;
-    width: 100vw;
     height: 100vh;
+    width: 100vw;
     margin: 0;
     padding: 0;  
 `;
 
+
+
 const MenuButton = styled(Button)`
     position: fixed;
+    display: flex;
     left: 10%;
     top: 40%;
     background-color: #ffff;
-    max-width: 150px;
+    max-width: 100px;
+   
     background: linear-gradient(177.07deg, #0C2FAD -41.5%, #7391FF 97.56%);
     border: 6px solid #415FCA;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -29,7 +33,7 @@ const MenuButton = styled(Button)`
     margin: 0;
     padding: 10px;
     font-weight: 600;
-    font-size: 30px;
+    font-size: calc(1rem + 1.24wv);
     text-align: center;
     color: #FFFFFF;
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -37,12 +41,13 @@ const MenuButton = styled(Button)`
 
 
 const MenuNav = styled(Button)`
+    display: flex;
     background: #415FCA;
     font-family: 'Orbitron';
     font-style: normal;
-    width: 250px;
+    max-width: 250px;
     font-weight: 500;
-    font-size: 27px;
+    font-size: calc(1rem + 2.24wv);
     line-height: 34px;
     text-align: center;
     color: #ffff;
@@ -70,22 +75,23 @@ class ImageMenu extends Component {
     render() {
         return (
 
-            <TransformWrapper alignmentAnimation={alignmentAnimation}  centerOnInit={true}
+            <TransformWrapper alignmentAnimation={alignmentAnimation} centerOnInit={true}
                               pinch={pinch} wheel={wheel} initialScale={1.6}>
 
                 <TransformComponent>
                     <DraggableImg>
                             <MenuNav style={{position: "fixed", right: "0", borderRadius: "0px 0px 0px 26px"}}>
-                             <span style={{padding: "10px"}}>Settings</span>
-                            <FontAwesomeIcon  style={{height: "50px", color: "#FFF"}} icon={faCog}/>
-                            </MenuNav >
+                                <span style={{padding: "10px"}}>Settings</span>
+                                <FontAwesomeIcon style={{height: "50px", color: "#FFF"}} icon={faCog}/>
+                            </MenuNav>
                             <MenuNav>
                                 <span style={{padding: "10px"}}>About project</span>
-                             <FontAwesomeIcon  style={{height: "50px", color: "#FFF"}} icon={faHockeyPuck}/>
+                                <FontAwesomeIcon style={{height: "50px", color: "#FFF"}} icon={faHockeyPuck}/>
                             </MenuNav>
-                        <Link href="/trade-cards/buy-cards"><MenuButton>TRADE CARDS</MenuButton></Link>
-                        <Link href="/manage-team/set-lineups"><MenuButton style={{left: "70%"}}>MANAGE TEAM</MenuButton></Link>
-                        <Link href="/game"><MenuButton style={{left: "40%"}}>PLAY GAME</MenuButton></Link>
+                            <Link href="/trade-cards/buy-cards"><MenuButton>TRADE CARDS</MenuButton></Link>
+                            <Link href="/manage-team/set-lineups"><MenuButton style={{left: "70%"}}>MANAGE
+                                TEAM</MenuButton></Link>
+                            <Link href="/game"><MenuButton style={{left: "40%"}}>PLAY GAME</MenuButton></Link>
                     </DraggableImg>
                 </TransformComponent>
             </TransformWrapper>
