b5e8a0bbf1fddbc4d4a713e1799dd3797a93d9e3
savbeM <mark.selihov2458@gmail.com>
2022-04-06 21:17:52 +0300

    lineups

--- components/styled-components.js
+++ components/styled-components.js
@@ -13,7 +13,7 @@ export const CircleBtn = styled(Button)`
   }
 `
 export const PlayingCard = styled.div`
-  background-image: url(${ props => props.image});
+  ${props => props.isDefined ? `background-image:url(${props.image})` : "background: #ffff"};
   background-size: cover;
   height: 125px;
   width: 80px;
@@ -46,36 +46,3 @@ export const PlayingCard = styled.div`
   }
 `
 
-export const UnselectedCard = styled.div`
-  background: black;
-  background-size: cover;
-  height: 125px;
-  width: 80px;
-  padding: 0;
-  margin: 0;
-  &, & img {
-    border-radius: 10px;
-  }
-  &.goalie {
-    margin-top: 140px;
-  }
-  &.goalie-game {
-    margin-top: 220px;
-  }
-  &.border {
-    border: 1px solid black !important;
-  }
-  &.bench img {
-    border: 3px solid white !important;
-  }
-  &.sm, &.sm img {
-    height: 90px;
-    width: 65px;
-  }
-  &.bottom-left {
-    margin-left: 40px;
-  }
-  &.bottom-right {
-    margin-right: 40px;
-  }
-`
--- pages/manage-team/set-lineups.jsx
+++ pages/manage-team/set-lineups.jsx
@@ -2,11 +2,8 @@ import {Container, Row, Col, Button} from "react-bootstrap";
 import styled from "styled-components";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeft, faArrowUp, faArrowDown, faTimes} from "@fortawesome/free-solid-svg-icons";
-import {CircleBtn, PlayingCard, UnselectedCard} from "../../components/styled-components";
+import {CircleBtn, PlayingCard} from "../../components/styled-components";
 import {useEffect, useState} from "react";
-
-
-
 
 
 const SContainer = styled(Container)`
@@ -41,49 +38,134 @@ const ActionButton = styled(Button)`
 export default function SetLineup() {
 
 
-
-    const [allPlayingCards, setAllPlayingCards] = useState( [
-        {cardName: 1, cardImage: "/card-back-blue.png"},
-        {cardName: 2, cardImage: "/card-back-yellow.png"},
-        {cardName: 3, cardImage: "/card-back-blue.png"},
-        {cardName: 4, cardImage: "/card-back-yellow.png"},
-        {cardName: 5, cardImage: "/card-back-blue.png"},
-        {cardName: 6, cardImage: "/card-back-blue.png"},
-        {cardName: 7, cardImage: "/card-back-blue.png"},
+    const [allPlayingCards, setAllPlayingCards] = useState([
+        {cardName: 1, cardImage: "/card-back-blue.png", position: null},
+        {cardName: 2, cardImage: "/card-back-yellow.png", position: null},
+        {cardName: 3, cardImage: "/card-back-blue.png", position: null},
+        {cardName: 4, cardImage: "/card-back-yellow.png", position: null},
+        {cardName: 5, cardImage: "/card-back-blue.png", position: null},
+        {cardName: 6, cardImage: "/card-back-blue.png", position: null},
+        {cardName: 7, cardImage: "/card-back-blue.png", position: null},
 
     ])
 
     const [activeCards, setActiveCards] = useState([
-        {cardName: 8, cardImage: "/card-back-blue.png", position: 1},
-        {cardName: 9, cardImage: "/card-back-yellow.png",position: 2},
-        {cardName: 10, cardImage: "/card-back-blue.png",position: 3},
+        {cardName: "unselected1", cardImage: "ffff", position: 1, isDefined: false,},
+        {cardName: "unselected2", cardImage: "ffff", position: 2,isDefined: false},
+        {cardName: "unselected3", cardImage: "ffff", position: 3,isDefined: false},
+        {cardName: "unselected4", cardImage: "ffff", position: 4,isDefined: false},
+        {cardName: "unselected5", cardImage: "ffff", position: 5,isDefined: false},
     ])
 
     const [selectedCard, setSelectedCard] = useState()
-    const [selectedActiveCard, setSelectedActiveCard] = useState()
+    const [selectedActiveCard, setSelectedActiveCard] = useState([])
 
     useEffect(() => {
 
-        if(selectedCard && selectedActiveCard){
-            setAllPlayingCards([...allPlayingCards, activeCards.find(e=> e.cardName === selectedActiveCard)])
-            setActiveCards([...activeCards, allPlayingCards.find(e => e.cardName === selectedCard)])
-            setActiveCards(items => items.filter(filt => filt.cardName !== selectedActiveCard))
-            setAllPlayingCards(items => items.filter(filt => filt.cardName !== selectedCard))
-            setSelectedCard(null)
+        if (selectedCard && selectedActiveCard && selectedActiveCard.length === 1 && selectedActiveCard[0].isDefined === true) {
+
+                let findedActiveCard = activeCards.find(e => e.cardName === selectedActiveCard[0].card)
+                let findedAllCard = allPlayingCards.find(e => e.cardName === selectedCard.card)
+                setAllPlayingCards([...allPlayingCards, {position: null, cardName: findedActiveCard.cardName, cardImage: findedActiveCard.cardImage}])
+                setActiveCards([...activeCards, {...findedAllCard, position: findedActiveCard.position, isDefined: true}])
+                setActiveCards(items => items.filter(filt => filt.cardName !== selectedActiveCard[0].card))
+                setAllPlayingCards(items => items.filter(filt => filt.cardName !== selectedCard.card))
+                setSelectedCard(null)
+                setSelectedActiveCard(null)
+        }
+        else if(selectedActiveCard && selectedActiveCard.length > 1 ){
+
+            setActiveCards(activeCards.map(e => {
+                if(e.cardName === selectedActiveCard[0].card){
+                    return  {...e, position: selectedActiveCard[1].position }
+                }
+                else {return e}
+
+            }))
+            setActiveCards(activeCards.map(e => {
+                if(e.cardName === selectedActiveCard[1].card){
+                    return  {...e, position: selectedActiveCard[0].position }
+                }
+                else {return e}
+
+            }))
+
             setSelectedActiveCard(null)
         }
+        else if(selectedCard && selectedActiveCard && selectedActiveCard.length === 1 && selectedActiveCard[0].isDefined === false){
+            let findedAllCard = allPlayingCards.find(e => e.cardName === selectedCard.card)
+            setActiveCards(activeCards.map(e => {
+                if(e.position === selectedActiveCard[0].position){
+                    return  { position: e.position, cardImage: findedAllCard.cardImage, cardName: findedAllCard.cardName, isDefined: true}
+                }
+                else {return e}
 
+            }))
+            setAllPlayingCards(items => items.filter(filt => filt.cardName !== selectedCard.card))
+            setSelectedActiveCard(null)
+            setSelectedCard(null)
+
+        }
     }, [selectedCard, selectedActiveCard])
-
-    const selectActiveCard = (selectedCard) => {
-        setSelectedActiveCard(selectedCard)
-    }
-
-    const selectCard = (card) => {
-        setSelectedCard(card)
-    }
-
     console.log(activeCards)
+    console.log(selectedActiveCard)
+    const removeDuplicates = (arr) => {
+
+        const result = [];
+        const duplicatesIndices = [];
+
+
+        arr.forEach((current, index) => {
+
+            if (duplicatesIndices.includes(index)) return;
+
+            result.push(current);
+
+
+            for (let comparisonIndex = index + 1; comparisonIndex < arr.length; comparisonIndex++) {
+
+                const comparison = arr[comparisonIndex];
+                const currentKeys = Object.keys(current);
+                const comparisonKeys = Object.keys(comparison);
+
+
+                if (currentKeys.length !== comparisonKeys.length) continue;
+
+
+                const currentKeysString = currentKeys.sort().join("").toLowerCase();
+                const comparisonKeysString = comparisonKeys.sort().join("").toLowerCase();
+                if (currentKeysString !== comparisonKeysString) continue;
+
+
+                let valuesEqual = true;
+                for (let i = 0; i < currentKeys.length; i++) {
+                    const key = currentKeys[i];
+                    if ( current[key] !== comparison[key] ) {
+                        valuesEqual = false;
+                        break;
+                    }
+                }
+                if (valuesEqual) duplicatesIndices.push(comparisonIndex);
+
+            }
+        });
+        return result;
+    }
+
+    const selectActiveCard = (selectedCard, position, isDefined) => {
+        setSelectedActiveCard( selectedActiveCard && selectedActiveCard.length > 0 && selectedActiveCard.length < 2 ?
+            [...selectedActiveCard ,{card: selectedCard, position: position, isDefined: isDefined}]
+            :
+        [{card: selectedCard, position: position, isDefined: isDefined}]
+        )
+
+    }
+
+    const selectCard = (selectedCard, position) => {
+        setSelectedCard({card: selectedCard, position: position})
+    }
+
+
 
     return <SContainer fluid className='p-3'>
         <Row className='justify-content-between'>
@@ -104,24 +186,53 @@ export default function SetLineup() {
             <TopSelectedCard xs={8}>
                 <SelectedPlayingCard style={{width: "500px", margin: "0 auto"}}>
                     {activeCards.map(e => {
+                        if(e.position === 1 ) {
+                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} isDefined={e.isDefined}
+                                                                      onClick={() => selectActiveCard(e.cardName, 1)}/></Col>
+                        }
 
-                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} onClick={() => selectActiveCard(e.cardName)} /></Col>
+                    })}
+                    {activeCards.map(e => {
+                        if(e.position === 2) {
+                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} isDefined={e.isDefined}
+                                                                      onClick={() => selectActiveCard(e.cardName, 2, e.isDefined)}/></Col>
+                        }
+
+                    })}
+                    {activeCards.map(e => {
+                        if(e.position === 3) {
+                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} isDefined={e.isDefined}
+                                                                      onClick={() => selectActiveCard(e.cardName, 3, e.isDefined)}/></Col>
+                        }
 
                     })}
                 </SelectedPlayingCard>
                 <SelectedPlayingCard className='mt-3 mb-5 justify-content-around'>
-                    <Col className='col-auto'>
-                        <PlayingCard className='bottom-left'/>
-                    </Col>
-                    <Col className='col-auto'>
-                        <PlayingCard className='bottom-right'/>
-                    </Col>
+
+
+                    {activeCards.map(e => {
+                        if(e.position === 4 ) {
+                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} isDefined={e.isDefined}
+                                                                      onClick={() => selectActiveCard(e.cardName, 4, e.isDefined)}/></Col>
+                        }
+
+                    })}
+
+                    {activeCards.map(e => {
+                        if(e.position === 5) {
+                            return <Col key={e.cardName}><PlayingCard image={e.cardImage} isDefined={e.isDefined}
+                                                                      onClick={() => selectActiveCard(e.cardName, 5, e.isDefined)}/></Col>
+                        }
+
+                    })}
+
                 </SelectedPlayingCard>
                 <Row>
                     <Col xs={11}>
                         <Row>
                             {allPlayingCards.map(e => {
-                                return <Col key={e.cardName}><PlayingCard onClick={()=> selectCard(e.cardName)}  image={e.cardImage}/></Col>
+                                return <Col key={e.cardName}><PlayingCard isDefined={true}  onClick={() => selectCard(e.cardName, null,)}
+                                                                          image={e.cardImage}/></Col>
                             })}
 
                         </Row>
